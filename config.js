3c3d64bfd4000b95765c6a96a46cf36eccb6e6ed
Jose Santos <jsantos108@gmail.com>
2020-02-28 22:22:32 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '490427721637647',
+        clientSecret: 'c611042d54125b24c4619925bd0bcd9a'
+    }
 }
