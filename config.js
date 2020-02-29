41d7477adb682357f33714fda842c48df6c14d3a
JC Santos <jsantos108@users.noreply.github.com>
2020-02-28 22:46:32 -0500

    Hide id/keys

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
     'facebook': {
-        clientId: '490427721637647',
-        clientSecret: 'c611042d54125b24c4619925bd0bcd9a'
+        clientId: '######',
+        clientSecret: '######'
     }
 }
